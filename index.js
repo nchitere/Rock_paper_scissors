298044aaa0711f6c6235e13958500d5e3671d227
nchitere <nchitere@gmail.com>
2023-11-08 17:03:05 +0300

    First functional rock paper scissors iteration

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
-game = ['Rock','Paper','Scissors']
+game = ['rock','paper','scissors']
 function getComputerChoice() {
     let computerIndex = Math.floor(Math.random()*3)
     return game[computerIndex]
 }
-console.log(getComputerChoice())
 
-function getPlayerChoice() {
-    let playerIndex = Math.floor(Math.random()*3)
-    return game[playerIndex]
+const playerSelection = "paper";
+
+
+function playRound(getComputerChoice, PlayerChoice) {
+    // Normalize player selection to lowercase
+  // playerSelection = playerSelection.toLowerCase()
+
+    // Check if player selection is valid
+    if (!['rock', 'paper', 'scissors'].includes(playerSelection)) {
+      throw new Error(`Invalid player selection: ${playerSelection}`);
+    }
+  
+    // Get the winning combinations
+    const winningCombinations = {
+      rock: ['paper'],
+      paper: ['scissors'],
+      scissors: ['rock'],
+    };
+  
+    // Determine the winner
+    if (playerSelection === computerSelection) {
+      winner = 'Tie!';
+    } else if (winningCombinations[playerSelection].includes(computerSelection)) {
+      winner = 'You Win!';
+    } else {
+      winner = 'You Lose!';
+    }
+  
+    // Return the winner declaration
+    return `${winner} ${computerSelection} beats ${playerSelection}`;
 }
-console.log(getPlayerChoice())
+
+
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
